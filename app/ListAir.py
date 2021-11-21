3de5ce436406955456e5453f3d377899bd7e58fc
akshxl09 <59478929+akshxl09@users.noreply.github.com>
2021-11-22 02:11:55 +0900

    Update ListAir.py

--- app/ListAir.py
+++ app/ListAir.py
@@ -9,9 +9,14 @@ list = Blueprint('list',__name__)
 
 @list.route('/get_airpolution')
 def get_airpolution():
+
+    loc = request.args.get("loc")
+#    print(loc)
+
+
     url = 'http://openapi.seoul.go.kr:8088/6d704f6276616b7339367762736a70/json/ListAirQualityByDistrictService/1/25/'
     data = requests.get(url).json()
-    pprint(data['ListAirQualityByDistrictService']['row'])
+#    pprint(data['ListAirQualityByDistrictService']['row'])
 
     cur = MongoClient(os.environ['MYAIR_MONGODB'])
     db_name = 'Myair'
@@ -19,8 +24,16 @@ def get_airpolution():
     
     # Create collection
     col = cur[db_name][col_name]
-    col.insert_many(data['ListAirQualityByDistrictService']['row'])
+    #col.insert_many(data['ListAirQualityByDistrictService']['row'])
 
-
-
-    return redirect('/')
+    i=0
+    result = data['ListAirQualityByDistrictService']['row']
+    while result[i]["MSRSTENAME"] != loc:
+        i += 1               
+    
+    pprint(result[i])
+    
+    return jsonify(
+        result = 'success',
+        data = result[i]
+    )
